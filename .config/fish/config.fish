02a96b4eba1413561a810d2adfee16e8c7fa7b30
Iago Nunes <6692103+iago@users.noreply.github.com>
2021-12-24 00:29:55 -0300

    fix: remove duplicate virtual environment on fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,4 @@ eval /Users/colaborador/opt/miniconda3/bin/conda "shell.fish" "hook" $argv | sou
 # <<< conda initialize <<<
 alias lx="exa -l -h --git"
 alias gitdotfiles="git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
+set -x VIRTUAL_ENV_DISABLE_PROMPT 1
