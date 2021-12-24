6fed9a1530f52255e30f677023c6c959d328d90e
Iago Nunes <6692103+iago@users.noreply.github.com>
2021-12-23 22:12:02 -0300

    feat: add fish and oh my fish configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/colaborador/opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+alias lx="exa -l -h --git"
+alias gitdotfiles="git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
